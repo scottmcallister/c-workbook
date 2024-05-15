ccf915951ed3f89f8240393b3aaad56fa2f60369
Scott <mr.scott.mcallister@gmail.com>
2024-05-15 00:58:42 -0300

    add longest line and nth power programs

--- 1/power.c
+++ 1/power.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
+int power(int m, int n);
+
 int main() {
-  // TODO
+  int i;
+
+  for (i = 0; i < 10; ++i)
+    printf("%d %d %d\n", i, power(2, 1), power(-3, i));
+
+  return 0;
+}
+
+int power(int base, int n) {
+  int p;
+
+  for (p = 1; n > 0; --n)
+    p = p * base;
+  return p;
 }
