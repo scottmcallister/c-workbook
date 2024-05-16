6440232cdab9f576c5ab049d770a3a8b6080f0a7
Scott <mr.scott.mcallister@gmail.com>
2024-05-16 00:10:33 -0300

    reverse longest line with new function

--- 1/longest_line.c
+++ 1/longest_line.c
@@ -2,6 +2,7 @@
 #define MAXLINE 1000
 
 int get_line(char line[], int maxline);
+void reverse(char to[], char from[]);
 void copy(char to[], char from[]);
 
 int main() {
@@ -9,6 +10,7 @@ int main() {
   int max;               // max length seen so far
   char line[MAXLINE];    // current input line
   char longest[MAXLINE]; // longest line saved here
+  char reversed[MAXLINE]; // reversed longest line saved here
 
   max = 0;
   while ((len = get_line(line, MAXLINE)) > 0) {
@@ -17,8 +19,9 @@ int main() {
       copy(longest, line);
     }
   }
+  reverse(reversed, longest);
   if (max > 0)
-    printf("%s", longest);
+    printf("%s\n", reversed);
   return 0;
 }
 
@@ -41,4 +44,18 @@ void copy(char to[], char from[]) {
   i = 0;
   while ((to[i] = from[i]) != '\0')
     ++i;
+}
+
+void reverse(char to[], char from[]) {
+ 
+  // count the length of the string
+  int i;
+  for (i = 0; from[i] != '\0'; ++i)
+    ;
+  --i; // skip the null character
+  // reverse the string
+  int j;
+  for (j = 0; i >= 0; ++j, --i)
+    to[j] = from[i];
+
 }
