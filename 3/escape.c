03f46b57a4f1984dbb810d6b86315d65c8a6a107
Scott <mr.scott.mcallister@gmail.com>
2024-05-23 23:37:00 -0300

    fix escape function

--- 3/escape.c
+++ 3/escape.c
@@ -1,33 +1,64 @@
 #include <stdio.h>
 
-void escape(char[] s, char[] t);
+void escape(char * s, char * t);
 
 int main()
 {
-  char[] str = "Hello there";
-  char[] output = "           ";
+  char str[50] = "Hello \t there\n";
+  char output[51];
+  
+  printf("Original string: %s\n", str);
 
-  escape(str, output);
+  escape(output, str);
+
+  printf("Escaped string: %s\n", output);
+
   return 0;
 }
 
-void escape(char[] s, char[] t)
+// escape: converts characters like newline and tab into visible escape sequences
+void escape(char * s, char * t)
 {
-  int i = 0;
-  char curr = s[i];
+  int i, j;
+  i = j = 0;
 
-  while(s >= 'a') {
-    switch(curr) {
+  while(t[i] != '\0') {
+    switch(t[i]) {
       case '\n':
-        t[i] = '\n';
+        s[j++] = '\\';
+        s[j] = 'n';
         break;
       case '\t':
-        t[i] = '\n';
+        s[j++] = '\\';
+        s[j] = 't';
         break;
       default:
-        t[i] = curr
+        s[j] = t[i];
+        break;
     }
+    j++;
     i++;
-    curr = s[i];
   }
+  s[j] = '\0';
 }
+
+// void escape(char[] s, char[] t)
+// {
+//   int i = 0;
+//   char curr = s[i];
+// 
+//   while(s >= 'a') {
+//     switch(curr) {
+//       case '\n':
+//         t[i] = '\n';
+//         break;
+//       case '\t':
+//         t[i] = '\n';
+//         break;
+//       default:
+//         t[i] = curr
+//     }
+//     i++;
+//     curr = s[i];
+//   }
+// }
