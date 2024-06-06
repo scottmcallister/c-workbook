a10ba4b3e057086aa4ec440d6ab76d219f894ed6
Scott <mr.scott.mcallister@gmail.com>
2024-06-05 23:52:11 -0300

    make function to check if end of string matches substring

---
+++ 5/strend.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+#define MAXLEN 20
+
+int strend(char *s, char *t);
+
+int main()
+{
+  char haystack[MAXLEN] = "This is The End";
+  char needle[MAXLEN] = "End";
+
+  int found = strend(haystack, needle);
+  printf("found: %d\n", found);
+
+  return 0;
+}
+
+
+int strend(char *s, char *t)
+{
+  int len = 0;
+  int end_match = 0;
+  // go to end of s
+  while (*s != '\0')
+    s++;
+  while (*t != '\0') {
+    t++;
+    len++;
+  }
+  int i;
+  for (i = 0; i < len; i++) {
+    if (*s != *t) {
+      end_match = 1;
+    }
+    s--;
+    t--;
+  }
+  return end_match;
+}
